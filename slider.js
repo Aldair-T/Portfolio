b36bca350770738627f0d3ce406e724ee04a6ee2
Aldair-T <atorreq@fi.uba.ar>
2023-03-24 02:56:15 -0300

    Se agrego contenido a proyectos y form de contacto

--- slider.js
+++ slider.js
@@ -1,3 +1,4 @@
+/* Slider configuracion */
 const slider = document.querySelector("#slider");
 let sliderSeccion = document.querySelectorAll(".slider_seccion");
 let sliderSeccionLast = sliderSeccion[sliderSeccion.length-1];
@@ -40,4 +41,26 @@ btnDer.addEventListener('click', function(){
 
 setInterval(function(){
     moverDer();
-}, 5000);
+}, 4000);
+
+/* Formulario Configuracion */
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+
+    if (response.ok) {
+        this.reset()
+        alert('Gracias por contactarme :)')
+    }
+}
